0ab8353f1be44378d4930a72df9643ec51838bb2
Dhiyaan <nirmaldhiyaan@gmail.com>
2025-10-11 17:32:55 -0500

    add listen and simple map

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <unordered_map>
+#include <string>
 
 int main() {
+    std::unordered_map<std::string, std::string> store;
+
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        std::cerr << "socket failed" << std::endl;
         return 1;
     }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
@@ -16,11 +22,14 @@ int main() {
     addr.sin_port = htons(7878);
 
     if (bind(server_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        std::cerr << "bind failed" << std::endl;
         return 1;
     }
 
-    std::cout << "socket bound successfully" << std::endl;
+    if (listen(server_fd, 10) < 0) {
+        return 1;
+    }
+
+    std::cout << "server listening on port 7878" << std::endl;
     close(server_fd);
     return 0;
 }
