2f000f729b007e6b2846838e9bda3c1576fd6153
Dhiyaan <nirmaldhiyaan@gmail.com>
2025-10-19 20:10:22 -0500

    add aof recovery

--- src/persistence.hpp
+++ src/persistence.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <fstream>
 #include <string>
+#include <unordered_map>
+#include <sstream>
 
 class AOFWriter {
 public:
@@ -13,6 +15,35 @@ public:
         file.flush();
     }
 
+    void log_delete(const std::string& key) {
+        file << "DEL " << key << "\n";
+        file.flush();
+    }
+
 private:
     std::ofstream file;
+};
+
+class AOFReader {
+public:
+    explicit AOFReader(const std::string& filename) : filename(filename) {}
+
+    void recover(std::unordered_map<std::string, std::string>& store) {
+        std::ifstream file(filename);
+        std::string line;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::string cmd, key, value;
+            iss >> cmd >> key;
+            if (cmd == "SET") {
+                iss >> value;
+                store[key] = value;
+            } else if (cmd == "DEL") {
+                store.erase(key);
+            }
+        }
+    }
+
+private:
+    std::string filename;
 };
